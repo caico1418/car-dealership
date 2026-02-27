246c3d157d18e458a53e9953d8f142abe75e9a60
Edgar Gonzalez <glez77104@gmail.com>
2026-02-26 22:29:51 -0600

    Se agregar la funcion findAll en services

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -1,19 +1,23 @@
 import { Controller, Get, Param } from '@nestjs/common';
 import { findIndex } from 'rxjs';
+import { CarsService } from './cars.service';
 
 @Controller('cars')
 export class CarsController {
-    private cars = ["Toyota", "Honda", "Jeep"];
+    
+    constructor(
+        private readonly carsService: CarsService
+    ){}
 
     @Get()
     getAllCars() {
-        return this.cars;
+        return this.carsService.findAll();
     }
 
-    @Get(':id')
+   /*  @Get(':id')
     getCarById( @Param('id') id: string ) {
        console.log({ id });
        return this.cars[id];
        
-    }
+    } */
 }
--- src/cars/cars.service.ts
+++ src/cars/cars.service.ts
@@ -18,5 +18,9 @@ export class CarsService {
             brand: 'Jeep',
             model: 'Cherokee'
         },
-    ]
+    ];
+
+    findAll() {
+        return this.cars;
+    }
 }
