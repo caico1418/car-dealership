6fb7f79e85b3d7a6ffa2c0d139f4ff5c22f6f5ac
Edgar Gonzalez <glez77104@gmail.com>
2026-02-26 21:51:11 -0600

    Enpoint para obtener un carro por id

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
+import { findIndex } from 'rxjs';
 
 @Controller('cars')
-export class CarsController {}
+export class CarsController {
+    private cars = ["Toyota", "Honda", "Jeep"];
+
+    @Get()
+    getAllCars() {
+        return this.cars;
+    }
+
+    @Get(':id')
+    getCarById( @Param('id') id: string ) {
+       console.log({ id });
+       return this.cars[id];
+       
+    }
+}
