a58bebfba962afc2b4fcf98e5de46d7d16cef720
Edgar Gonzalez <glez77104@gmail.com>
2026-02-28 22:22:50 -0600

    Se agrega el usePipes

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -1,5 +1,6 @@
-import { Body, Controller, Delete, Get, Param, ParseIntPipe, ParseUUIDPipe, Patch, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, ParseUUIDPipe, Patch, Post, UsePipes, ValidationPipe } from '@nestjs/common';
 import { CarsService } from './cars.service';
+import { CreateCarDto } from './dto/create-car.dto';
 
 @Controller('cars')
 export class CarsController {
@@ -20,8 +21,9 @@ export class CarsController {
     }
 
     @Post()
-    createCar( @Body() body: any ) {
-        return body;
+    @UsePipes( ValidationPipe )
+    createCar( @Body() createCardDto: CreateCarDto ) {
+        return createCardDto;
     } 
 
     @Patch(':id')
