01a55defb58b3f6ecc39994384aab117bb0e5ad4
Edgar Gonzalez <glez77104@gmail.com>
2026-03-02 19:37:20 -0600

    Se agrega un nuevo enpoint

---
+++ src/brands/brands.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@nestjs/common';
+import { v4 as uuid } from "uuid";
+
+import { CreateBrandDto } from './dto/create-brand.dto';
+import { UpdateBrandDto } from './dto/update-brand.dto';
+import { v4 } from 'uuid';
+
+@Injectable()
+export class BrandsService {
+  create(createBrandDto: CreateBrandDto) {
+    return 'This action adds a new brand';
+  }
+
+  findAll() {
+    return `This action returns all brands`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} brand`;
+  }
+
+  update(id: number, updateBrandDto: UpdateBrandDto) {
+    return `This action updates a #${id} brand`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} brand`;
+  }
+}
